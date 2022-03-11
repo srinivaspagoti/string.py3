b3e392bc84c20f2abe82d30b4431463e6f49f466
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-11 17:23:15 +0530

    Add files via upload

---
+++ strings_mathod.py
@@ -0,0 +1,31 @@
+# print(ord('A'))
+# print(ord("S"))
+# print(ord('Z'))
+# print(ord('q'))
+# print(ord("g"))
+# print(ord('/'))
+# print(ord('!'))
+
+# print(chr(55))
+# print(chr(88))
+# print(chr(128))
+# print(chr(19))
+
+# a=bin(7)
+# print(a,type(a))
+# d=0b01010011
+# print(d,type(d))
+
+# l=oct(58)
+# print(l,type(l))
+# u=0o755
+# print(u,type(u))
+
+# t=hex(1478)
+# print(t,type(t))
+# w=0x2589
+# print(w,type(w))
+
+p="I lOvE YoU SrInIvAs"
+print(p.swapcase())
+
